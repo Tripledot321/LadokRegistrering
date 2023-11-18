8a09a7439d70411a67eefe97f4773ad9e837765d
Tripledot321 <joakim.gremlin@hotmail.com>
2023-11-18 20:24:11 +0100

    Tar bort skräpkod

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const productRoutes = require('./api/routes/products');
 
 mongoose.connect('mongodb+srv://user123:4KnNlLNdbNcnRnCR@cluster0.sdivnpi.mongodb.net/', {
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-app.use('/products', productRoutes);
 
 module.exports = app;
