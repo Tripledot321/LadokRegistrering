8bb8e1158edd2f16ade15dceac39bfa847aa7009
Tripledot321 <joakim.gremlin@hotmail.com>
2023-11-25 18:17:13 +0100

    1

--- api/routes/Ladok.js
+++ api/routes/Ladok.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-const ItsStudent = require('../models/LadokDB');
+const LadokStudentResult = require('../models/LadokDB');
