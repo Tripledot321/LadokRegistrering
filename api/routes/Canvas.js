108e42d8fe6f18916f90a0ffaae855d2e79b0510
jgaffa <93146229+jgaffa@users.noreply.github.com>
2023-11-29 18:24:02 +0100

    horisontell pres av studentdata +  betygdropdown

--- api/routes/Canvas.js
+++ api/routes/Canvas.js
@@ -85,48 +85,4 @@ router.get('/get_StudentList/', async (req, res) => {
   res.status(500).send('Internal Server Error');
 }
 });
-/*
-router.get('/get_StudentResult', async (req, res) => {
-  try {
-    const studentId = req.query.studentId;
-    console.log('Input studentId:', studentId);
-
-    // query db for student results
-    const studentResults = await CanvasStudentResult.find({ studentId })
-      .populate({
-        path: 'courseId',
-        select: 'courseName',
-      })
-      .populate({
-        path: 'grades.assignmentId', // Populate the assignmentId field
-        model: 'Assignment', // Specify the model for assignmentId
-      });
-
-    if (!studentResults || studentResults.length === 0) {
-      return res.status(404).json({
-        message: 'Inga resultat för valt studentID.',
-      });
-    }
-
-    // response
-    const formattedResults = studentResults.map(result => ({
-      CourseCode: result.courseId && result.courseId._id,
-      AssignmentID: result.grades.map(assignment => ({
-        _id: assignment.assignmentId && assignment.assignmentId._id, // Use assignmentId
-        assignment: assignment.assignmentId && assignment.assignmentId.assignment,
-        grade: assignment.grade,
-      })),
-      Grade: result.Grade,
-    }));
-
-    res.status(200).json({
-      studentId,
-      studentResults: formattedResults,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-*/
 module.exports = router;
